b9de3a38e8fb61bd1923edd14f0a7e1a00040197
CCKsuiyila <1453787167@qq.com>
2017-04-16 09:54:21 +0700

    递归改迭代,然后2还没改,迟点回来复习顺便改了  jump game55

--- 55.cpp
+++ 55.cpp
@@ -1,3 +1,30 @@
+//交流的过程中发现有迭代版   自己先改一改试一试
+//虽然没有和别人的那样  又有全局又有局部,但是也听过了
+//这次突然有了点想法    根据复杂度来判断要遍历几遍
+
+//然后2那个需要记录跳几次的也可以用循环    迟点改吧
+//  9 ms   93.70%
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+		if(nums.size()<=1){
+			return true;
+		}
+        int current = 0;
+		int i = 0;
+		for(;i<=current;++i){
+			if(nums[i]+i>current){
+				current =nums[i]+i;
+				if(current>=nums.size()-1){
+					return true;
+				}
+			}
+		}
+		return false;
+    }
+};
+
+/*
 //这个直接从Jump Game||改过来了   同时修改了一些小细节
 //小细节在下面
 // 12 ms 62.89% 
@@ -36,3 +63,4 @@ public:
 		}
     }
 };
+*/
